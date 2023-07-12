f2efde3fc950104bd42296ba05c9e3431af91704
Андрей Кузнецов <blacksilver@ro.ru>
2023-07-12 09:49:39 +0300

    Добавление Fram'a для маршрутизация между страницами, добавление страницы "Сегодня"

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,28 +1,19 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
+﻿using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using ToDo.Pages;
 
 namespace ToDo
 {
-    /// <summary>
-    /// Логика взаимодействия для MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
+        public static Frame MainFrameInstance;
+
         public MainWindow()
         {
             InitializeComponent();
+
+            MainFrameInstance = MainFrame;
+            MainFrameInstance.Navigate(new TodayPage());
         }
     }
 }
