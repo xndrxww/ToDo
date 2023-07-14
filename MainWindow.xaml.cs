1534b9eb6f9b873b752d8acf61f7840c2dbe314d
Андрей Кузнецов <blacksilver@ro.ru>
2023-07-14 09:41:13 +0300

    Сериализация и десериализация задач в xml

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,6 +1,9 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
 using System.Windows;
 using System.Windows.Controls;
+using System.Xml.Serialization;
 using ToDo.Pages;
 
 namespace ToDo
@@ -9,13 +12,41 @@ namespace ToDo
     {
         public static Frame MainFrameInstance;
         public static List<Models.Task> TasksList = new List<Models.Task>();
+        private string FileName = "tasks.xml";
 
         public MainWindow()
         {
             InitializeComponent();
+            DeserializeTasks();
 
             MainFrameInstance = MainFrame;
             MainFrameInstance.Navigate(new TodayPage());
+        }
+
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            if (TasksList != null)
+            {
+                XmlSerializer xmlSerializer = new XmlSerializer(TasksList.GetType());
+
+                using (FileStream fs = new FileStream(FileName, FileMode.OpenOrCreate))
+                {
+                    xmlSerializer.Serialize(fs, TasksList);
+                }
+            }
+        }
+
+        private void DeserializeTasks()
+        {
+            if (File.Exists(FileName))
+            {
+                XmlSerializer xmlSerializer = new XmlSerializer(TasksList.GetType());
+
+                using (FileStream fs = new FileStream("tasks.xml", FileMode.OpenOrCreate))
+                {
+                    TasksList = (List<Models.Task>)xmlSerializer.Deserialize(fs);
+                }
+            }
         }
     }
 }
